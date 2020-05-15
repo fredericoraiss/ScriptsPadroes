765f72fbcc18ba87ae57b5b33f9d0e2457fee828
Frederico Raiss <fred.raiss@hotmail.com>
2020-05-15 04:29:21 -0300

    Creat Folders
    Script simples para criar pastas inicias.

---
+++ CreateANewProject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateANewProject : ScriptableWizard
+{
+    public static List<string> principaisPastasNomes = new List<string> {"_Resources","_Scenes","_Scripts", "Prefabs" };
+
+
+    private void OnEnable()
+    {
+            
+    }
+
+
+    [MenuItem("Tools/Pastas Iniciais")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Create Project Folders", typeof(CreateANewProject), "Create");
+    }
+
+    private void OnWizardCreate()
+    {
+        foreach (string pasta in principaisPastasNomes)
+        {
+            string guid = AssetDatabase.CreateFolder("Assets", pasta);
+            string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+        }
+        AssetDatabase.Refresh();
+    }
+}
