8998b63c0ad277012afba8dff6d33a1ecdc73597
Frederico Raiss <fred.raiss@hotmail.com>
2020-10-18 07:23:37 -0300

    Add files via upload
    Mudar versão

---
+++ VersionUpdater.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class VersionController : EditorWindow
+{
+    static string currentVersion;
+
+    [MenuItem("Tools/Update Version &#v")]
+    static void Init()
+    {
+        currentVersion = Application.version;
+        EditorWindow.GetWindow(typeof(VersionController), true, "Update Version").Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Atualizar Versão da Build");
+
+        GUILayout.Space(15);
+        currentVersion = EditorGUILayout.TextArea(currentVersion);
+
+        if (GUILayout.Button("Atualizar"))
+        {
+            PlayerSettings.bundleVersion = currentVersion;
+            Debug.Log($"Versão do projeto atualizada para: {Application.version}");
+            this.Close();
+        }
+    }
+}
