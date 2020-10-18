c49222f1e1048ee3324ad473a7beacc8484eb27c
Frederico Raiss <fred.raiss@hotmail.com>
2020-10-18 07:16:53 -0300

    Add files via upload
    Singleton Generico

---
+++ SingletonMonoBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Get;
+    protected virtual void Awake()
+    {
+        if (Get == null)
+        {
+            Get = this as T;
+        }
+        else if (Get != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ SingletonMonoBehaviorPersistent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehaviourPersistent<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Get;
+    protected void Awake()
+    {
+        if (Get == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Get = this as T;
+        }
+        else if (Get != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
